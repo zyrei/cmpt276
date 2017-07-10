802aaef20ff5b228a3828a22883e58792d7544cd
nehamel <andre.anthony.lezama@gmail.com>
2017-07-10 16:27:50 -0700

    Add files via upload

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+# Use strong_parameters for attribute whitelisting
+# Be sure to update your create() and update() controller methods.
+
+  def browse
+    @users = User.where.not(id: current_user.id)
+  end 
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to log_in_url, :notice => "Signed up"
+  	else
+  		render "new"
+  	end
+  end
+  def user_params
+  	params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :age, :gender, :city, :descriptions, :avatar)
+  end
+  #def show
+end
---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ApplicationRecord
+	attr_accessor :password
+	before_save :encrypt_password
+
+	validates_confirmation_of :password
+	validates_presence_of :password, :on => :create
+	validates_presence_of :email
+	validates_uniqueness_of :email
+
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+
+	def encrypt_password
+		if password.present?
+			self.password_salt = BCrypt::Engine.generate_salt
+			self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+		end
+	end
+	
+	def self.authenticate(email, password)
+		user = find_by_email(email)
+		if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+			user
+		else
+			nil
+		end
+	end
+end
