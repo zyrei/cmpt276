b679279bd52436f89f0889fd5367de59c992b15d
Andre <andre.lezama@hotmail.com>
2017-07-11 19:05:41 -0700

    rerouted the navbar to devise login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  
   root 'a_thousand_word#home'
 
   get 'a_thousand_word/home'
@@ -12,15 +13,21 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
-  get "log_out" =>"sessions#destroy", :as => "log_out"
-  get "log_in" =>"sessions#new", :as => "log_in"
-  get "sign_up" =>"users#new", :as => "sign_up"
+  get "log_out" =>"sessions#destroy"#, :as => "log_out"
+  get "log_in" =>"sessions#new"#, :as => "log_in"
+  get "sign_up" =>"users#new"#, :as => "sign_up"
+  
   
   resources :users
   resources :sessions
   resources :profiles
 
-  devise_for :devise_users, controllers: { registration: 'devise_users/registration' }
+
+
+  devise_for :devise_users, controllers: { registration: 'devise_users/registrations' }
+
+
+
 
   #root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
