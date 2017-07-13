7008c5903189831cdccbd9fafe52e9031a3b8780
nncyyd <Nancydai.s2@gmail.com>
2017-07-12 17:41:30 -0700

    Merge branch 'master' of https://github.com/zyrei/cmpt276

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
+  get 'profiles/show'
+
   get "log_out" =>"sessions#destroy"#, :as => "log_out"
   get "log_in" =>"sessions#new"#, :as => "log_in"
   get "sign_up" =>"users#new"#, :as => "sign_up"
