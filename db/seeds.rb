0b53eb357f17b58f1280869c00a2cd67c740d342
nehamel <andre.anthony.lezama@gmail.com>
2017-07-21 15:49:14 -0700

    set up seeding for devise users & little more css

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+DeviseUser.create!(email: 'default@test.com', password: 'password', password_confirmation: 'password', nickname: "Kevin Smith", gender:'male', age: '25', city: 'Burnaby', description: 'im an average human' )
+for x in 0..9
+	DeviseUser.create!(email: "j#{x}hn@example.com", password: "123456", password_confirmation: "123456", nickname: "J#{x}hn Marsh#{x}ll", gender: 'male', age: "2#{x}", city: "Vancouver", description: "my name is j#{x}hn and i'm a skateboarder." )
+end
