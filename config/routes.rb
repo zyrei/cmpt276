04e821239ce11e56a18851a450f369c38e71c85a
nehamel <andre.anthony.lezama@gmail.com>
2017-07-21 15:32:37 -0700

    moved all css changes over, added logic to display a placeholder if images are missing, and set up activeadmin to read from the devise_user table as well as the old one

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   get 'profiles/show'
 
+  get 'admin', to: 'admin#index'
   get "log_out" =>"sessions#destroy"#, :as => "log_out"
   get "log_in" =>"sessions#new"#, :as => "log_in"
   get "sign_up" =>"users#new"#, :as => "sign_up"
