afbb778411c4ba4d2f2238a09f1210b2b34614e0
nehamel <andre.anthony.lezama@gmail.com>
2017-07-28 06:24:29 -0700

    fixed jamies map page & got 'city' field in DB to store address, geocoder converts to coords

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,16 @@
 class LocationsController < InheritedResources::Base
  
 
+ def edit
+ 	@user = DeviseUser.find(current_devise_user.id)
+ end
+
+ def update
+ 	@user = DeviseUser.find(current_devise_user.id)
+ 	@user.update_attributes(params[:address, :latitude, :longitude])
+ end
+
+
 def index
   if params[:search].present?
     @locations = Location.near(params[:search], 50, :order => :distance)
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
 
   get 'profiles/show'
 
+
+  get 'locate', to: 'locations#new'
   get 'admin', to: 'admin#index'
   get "log_out" =>"sessions#destroy"#, :as => "log_out"
   get "log_in" =>"sessions#new"#, :as => "log_in"
